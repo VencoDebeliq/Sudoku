7b724b14ce4cfafb2c2c682831bd39fc8c8dbc13
vencoDebeliq <vencarskibggaming@gmail.com>
2023-05-23 12:49:10 +0300

    erase button working now

--- Game.java
+++ Game.java
@@ -1,10 +1,8 @@
-package sudoku;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
+package sudoku;
 
 import java.awt.Color;
 import java.awt.Component;
